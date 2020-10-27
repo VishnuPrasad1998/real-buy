84e290821c20c523a4210fdff659215497c4efe7
VishnuPrasad1998 <vishnu1998prasad@gmail.com>
2020-10-27 14:17:06 +0530

    Updated UI

--- realbuy/settings.py
+++ realbuy/settings.py
@@ -120,6 +120,6 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-STATICFILES_DIR = [
+STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'realbuy/static')
 ]
