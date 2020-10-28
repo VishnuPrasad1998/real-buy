ca5875b441c92bc802a792635779fbcd801d628b
VishnuPrasad1998 <vishnu1998prasad@gmail.com>
2020-10-28 20:03:07 +0530

    Added a view for recent property listing

--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from listings.models import Listing
 # Create your views here.
 def index(request):
-    return render(request, 'pages/index.html')
+    recent_listings = Listing.objects.raw('SELECT * FROM listings_listing ORDER BY posting_date DESC')
+    context = {
+        'recent_listings': recent_listings
+    }
+    return render(request, 'pages/index.html', context)
