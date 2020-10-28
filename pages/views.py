5f3623113d7b97a56bf9066eff3dae0aa2db80c8
VishnuPrasad1998 <vishnu1998prasad@gmail.com>
2020-10-28 22:14:56 +0530

    Recent and Featured properties displayed dynamically on the landing page

--- pages/views.py
+++ pages/views.py
@@ -3,8 +3,11 @@ from django.http import HttpResponse
 from listings.models import Listing
 # Create your views here.
 def index(request):
+    featured_listings = Listing.objects.raw('SELECT * FROM listings_listing ORDER BY price DESC')
     recent_listings = Listing.objects.raw('SELECT * FROM listings_listing ORDER BY posting_date DESC')
+
     context = {
-        'recent_listings': recent_listings
+        'recent_listings': recent_listings,
+        'featured_listings': featured_listings
     }
     return render(request, 'pages/index.html', context)
