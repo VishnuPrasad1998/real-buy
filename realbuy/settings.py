71adc683faae081b25127b499b370466dd72887b
VishnuPrasad1998 <vishnu1998prasad@gmail.com>
2020-10-29 22:44:24 +0530

    Heroku error fixing

--- realbuy/settings.py
+++ realbuy/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'realbuy/static')
+    os.path.join(BASE_DIR, 'static')
 ]
 
 #Media Files
