ae5e9ab1dbfa63aef016ec1421cfce8aa64b2ea1
VishnuPrasad1998 <vishnu1998prasad@gmail.com>
2020-10-30 00:47:21 +0530

    Heroku Pipeline

--- realbuy/settings.py
+++ realbuy/settings.py
@@ -87,6 +87,9 @@ DATABASES = {
         'HOST': 'localhost'
     }
 }
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
