2fdeb6afc3c222bcd3e952cb940092ea320bb800
VishnuPrasad1998 <vishnu1998prasad@gmail.com>
2020-10-29 22:34:04 +0530

    Heroku error fixing

--- realbuy/settings.py
+++ realbuy/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -134,3 +135,5 @@ STATICFILES_DIRS = [
 #Media Files
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
