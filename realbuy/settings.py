e2df5d93128bdfd876e91c349606a96cbceb7391
VishnuPrasad1998 <vishnu1998prasad@gmail.com>
2020-10-29 23:09:03 +0530

    Heroku error fixing

--- realbuy/settings.py
+++ realbuy/settings.py
@@ -127,8 +127,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 #Media Files
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
