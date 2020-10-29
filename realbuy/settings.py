2fc7e22367fcaf6d8184b1d880e2c8210ae53c8c
VishnuPrasad1998 <vishnu1998prasad@gmail.com>
2020-10-29 23:16:33 +0530

    Heroku

--- realbuy/settings.py
+++ realbuy/settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 
