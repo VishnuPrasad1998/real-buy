827bc7e3767eee87fee0e6b8ff49a5a4fc7871c3
VishnuPrasad1998 <vishnu1998prasad@gmail.com>
2020-10-29 22:01:15 +0530

    Heroku error fixing

--- realbuy/urls.py
+++ realbuy/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.conf.urls import url, include
 from django.views.static import serve
 from django.conf.urls import url
 
