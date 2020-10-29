bc1b7fda1415f7043466af27cabeabeffbb3644d
VishnuPrasad1998 <vishnu1998prasad@gmail.com>
2020-10-30 00:51:07 +0530

    Heroku Pipeline

--- realbuy/settings.py
+++ realbuy/settings.py
@@ -128,8 +128,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 #Media Files
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
--- realbuy/urls.py
+++ realbuy/urls.py
@@ -24,8 +24,7 @@ urlpatterns = [
 
 
 
-
     path('', include('pages.urls')),
     path('listings/', include('listings.urls')),
     path('admin/', admin.site.urls),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
