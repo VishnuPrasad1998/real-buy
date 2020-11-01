bce9cce021cd0d9f25ef0cff2309b302e9d74626
VishnuPrasad1998 <vishnu1998prasad@gmail.com>
2020-11-02 00:33:05 +0530

    Created and migrated model for Contactus

--- contactus/models.py
+++ contactus/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+class Contactus(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.CharField(max_length=50)
+    phone = models.CharField(max_length=20)
+    message = models.TextField(max_length=500,blank=True)
+    contact_date = models.DateTimeField(default=datetime.now, blank=True)
+
+    def __str__(self):
+        return self.name
