4234ec39821f89af8d33d8c164a8500ddc525656
VishnuPrasad1998 <vishnu1998prasad@gmail.com>
2020-11-09 00:58:19 +0530

    Added Google Maps API

--- realbuy/static/js/main.js
+++ realbuy/static/js/main.js
@@ -1,3 +1,19 @@
 setTimeout(function() {
   $('#mgalert').fadeOut('slow');
 }, 3000);
+
+function initMap() {
+  // The location of Uluru
+  const uluru = { lat: 9.9312, lng: 76.2673 };
+  // The map, centered at Uluru
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 15,
+    center: uluru,
+  });
+
+  // The marker, positioned at Uluru
+  const marker = new google.maps.Marker({
+    position: uluru,
+    map: map,
+  });
+}
--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,20 @@
 setTimeout(function() {
   $('#mgalert').fadeOut('slow');
 }, 3000);
+
+function initMap() {
+  // The location of Uluru
+  const uluru = { lat: 9.9312, lng: 76.2673 };
+  // The map, centered at Uluru
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 10,
+    center: uluru,
+  });
+
+  // The marker, positioned at Uluru
+  const marker = new google.maps.Marker({
+    position: uluru,
+    map: map,
+    icon: 'Location_white.png'
+  });
+}
