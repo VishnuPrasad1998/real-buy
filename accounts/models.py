5c1a4b25df07af81d972e1a87e2d864cfa2ad70b
VishnuPrasad1998 <vishnu1998prasad@gmail.com>
2020-11-25 00:40:31 +0530

    Added feature for Cropping and Resizing

--- accounts/models.py
+++ accounts/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django_resized import ResizedImageField
+
 # Create your models here.
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    photo_user = models.ImageField(upload_to='photos/user/propic', blank=False, default='photos/2020/10/30/avatar.jpeg')
+    photo_user = ResizedImageField(size=[600, 600], upload_to='photos/user/propic', blank=False, default='photos/2020/10/30/avatar.jpeg')
     address = models.CharField(max_length=200, blank=False, default="sdsdsadsdasdasdasdsadasdsad")
     phone = models.CharField(max_length=200, blank=False, default="838848484")
 
