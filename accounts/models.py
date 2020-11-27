84b0afcf2f4837192100d576dec52d753fa3fb2a
VishnuPrasad1998 <vishnu1998prasad@gmail.com>
2020-11-27 09:59:06 +0530

    Fixed profile defaults

--- accounts/models.py
+++ accounts/models.py
@@ -6,8 +6,8 @@ from django_resized import ResizedImageField
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     photo_user = ResizedImageField(size=[600, 600], upload_to='photos/user/propic', blank=False, default='photos/2020/10/30/avatar.jpeg')
-    address = models.CharField(max_length=200, blank=False, default="sdsdsadsdasdasdasdsadasdsad")
-    phone = models.CharField(max_length=200, blank=False, default="838848484")
+    address = models.CharField(max_length=200, blank=False, default="Palarivattam, Cochin")
+    phone = models.CharField(max_length=200, blank=False, default="7839939939")
 
     def __str__(self):
         return self.user.username
