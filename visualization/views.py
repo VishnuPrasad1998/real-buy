438aceba9c7ea10a84f5f4884e14c5f4d5e2a8d6
VishnuPrasad1998 <vishnu1998prasad@gmail.com>
2020-12-01 13:14:13 +0530

    Replaced csv file from direct database data

--- visualization/views.py
+++ visualization/views.py
@@ -4,39 +4,42 @@ from matplotlib import pyplot as plt
 from django.contrib.auth.decorators import login_required
 from accounts.decorators import allowed_users
 from django.contrib.auth.models import Group 
-
+from django_pandas.io import read_frame
+from shortlist.models import Shortlist
 # Create your views here.
 @login_required(login_url='login')
 @allowed_users(allowed_roles=['admin'])
 def Visualize(request):
-	data = pd.read_csv('visualization/csv_files/Shortlist.csv')
-	data.head() 
-	df = pd.DataFrame(data)
+    data = Shortlist.objects.all()
 
-	df = df.loc[:,['listing']]
-	df.dropna( inplace=True)
+    df = read_frame(data)
+
+    print(df)
+    
+    df = df.loc[:,['listing']]
+    df.dropna( inplace=True)
 
 
-	count = df.count(axis=1)
+    count = df.count(axis=1)
 
 
-	df['count'] = count
+    df['count'] = count
 
-	df.columns = ['Listing', 'Count']
+    df.columns = ['Listing', 'Count']
 
-	df = df.groupby(["Listing"])["Count"].count().reset_index()
+    df = df.groupby(["Listing"])["Count"].count().reset_index()
 
-	listing = df['Listing'].head(len(df))
+    listing = df['Listing'].head(len(df))
 
-	count = df['Count'].head(len(df))
+    count = df['Count'].head(len(df))
 
-	fig = plt.figure(figsize =(7, 5))
+    fig = plt.figure(figsize =(7, 5))
 
-	plt.bar(listing[0:10], count[0:10], color='#2ca665') 
-	plt.xlabel('Listings')
-	plt.ylabel('Shortlisted Count')
+    plt.bar(listing[0:10], count[0:10], color='#2ca665') 
+    plt.xlabel('Listing')
+    plt.ylabel('Shortlisted Count')
 
-	plt.savefig('static/img/List_img.png')
+    plt.savefig('realbuy/static/img/List_img.png')
 
-	
-	return render(request, 'visualization/visual.html')
+    
+    return render(request, 'visualization/visual.html')
